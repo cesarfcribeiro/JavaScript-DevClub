4ea0b6f83ef544e785d5b22c735a0c8990904e54
cesarfcribeiro <cesarfcribeiro@gmail.com>
2025-04-15 14:21:41 -0300

    adicionando conteudo function

--- 4.ifElse.js
+++ 4.ifElse.js
@@ -1,4 +1,18 @@
-/*
+/* Controlador de fluxo
+    - If (se) -> Faça isso
+    - Else (se não)
 
-
+    Comparação: > maior que
+                < menor que
+                == igual
 */
+
+const notaAluno = 3;
+const notaCorte = 5;
+
+if (notaAluno > notaCorte) {
+    //executado se o if for verdadeiro
+  console.log("Aprovado");
+} else {
+  console.log("Reprovado");
+}
---
+++ 5.function.js
@@ -0,0 +1,14 @@
+/*  Funções
+
+    Trecho de codigo que é executado 
+        SOMENTE quando solicitado
+*/
+
+const folgado = "parente chato"
+console.log(folgado)
+
+function nomeNaTela() {
+    console.log("Cesar")
+}
+
+nomeNaTela()
