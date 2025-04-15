350b7a4e607261709e2c2d434b39e868e5decf86
cesarfcribeiro <cesarfcribeiro@gmail.com>
2025-04-15 15:03:10 -0300

    adicionei arquivo element

---
+++ 6.element-selection.js
@@ -0,0 +1,23 @@
+/*
+document --> HTML
+
+getElementById --> Trás UM elemento pelo ID  
+getElementByClassName --> Trás TODOS os elementos com essa classe  
+getElementByTagName --> Trás TODOS os elementos com essa TAG  
+getElementByName --> Trás TODOS os elementos com esse NAME  
+
+querySelector --> Trás UM elemento, o PRIMEIRO que encontrar  
+querySelectorAll --> Trás TODOS os elementos que encontrar 
+*/
+
+const input = document.getElementById("main-input")
+console.log(input)
+
+const elements = document.getElementsByClassName("paragraf-js")
+console.log(elements)
+
+const tag = document.getElementsByTagName("p")
+console.log(tag)
+
+const nome = document.getElementsByName("nome-completo")
+console.log(nome)
