ec0321fbd9edec41ba53015f2e398b148d2ec32b
cesarfcribeiro <cesarfcribeiro@gmail.com>
2025-04-15 09:12:21 -0300

    primeiro envio

---
+++ 1.variaveis.js
@@ -0,0 +1,11 @@
+/*
+Variáveis
+- let: pode alterar o valor quando quiser.
+- const: constante, valor não pode ser alterado.
+-var: descontinuado, não usar.
+
+*/
+
+let numeroAlunos = 40
+
+console.log(numeroAlunos)
---
+++ 2.tiposDeDados.js
@@ -0,0 +1,14 @@
+/*
+1. String -> Textos.
+
+*/
+
+const texto1 = "Textos 'Textos' Textos"
+let texto2 ='Textos Textos Textos'
+let texto3 =`Texto 
+com 
+crase` //Template literals ou Template String
+
+console.log(texto1)
+console.log(texto2)
+console.log(texto3)
