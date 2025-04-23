5e8265f3cdf3edfbe76ab928a526c5c816c058e0
cesarfcribeiro <cesarfcribeiro@gmail.com>
2025-04-23 13:59:12 -0300

    adicionei math

---
+++ 7.math.js
@@ -0,0 +1,26 @@
+/*Math -> Matemática
+ - pow(2,2) / potência / 2² = 4
+ - sqrt(25,2) / raiz / √25
+ - PI / 3.141592653589793
+ - ceil / teto -> arredondar pra cima
+ - floor / chão -> arredondar pra baixo
+ - random() / número aleatório entre 0 e 1
+*/
+
+const result = Math.pow(2,8)
+console.log(result)
+
+const result1 = Math.sqrt(25,2)
+console.log(result1)
+
+const result2 = Math.PI
+console.log(result2)
+
+const result3 = Math.ceil(3.14)
+console.log(result3)
+
+const result4 = Math.floor(3.14)
+console.log(result4)
+
+const result5 = Math.random()
+console.log(result5)
