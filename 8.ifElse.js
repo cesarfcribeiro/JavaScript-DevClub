2058909dc2bbd1431c526de71af3eb64cbc3e7a8
cesarfcribeiro <cesarfcribeiro@gmail.com>
2025-04-25 14:45:54 -0300

    adicionei operador ternario

---
+++ 8.ifElse.js
@@ -0,0 +1,18 @@
+const salary = 1000
+
+// if(salary < 3000) {
+//   console.log("O colaborador é Júnior")
+// } else if(salary >= 3000 && salary < 10000) {
+//   console.log("Ele é Sênior")
+// } else {
+//   console.log("Ele é diretor")
+// }
+
+// IF salary < 3000 && console.log("O colaborador é Júnior")
+
+// IF/ELSE => salary < 3000 ? console.log("O colaborador é Júnior") : console.log("Ele é Sênior")
+
+// ELSE IF
+salary < 3000 ? console.log("O colaborador é Júnior") : 
+salary >= 3000 && salary < 10000 ? console.log("Ele é Sênior") : 
+console.log("Ele é diretor")
